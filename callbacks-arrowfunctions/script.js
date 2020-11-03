45a8aa0e7275dbd2a3aac96760680bf59ad198e6
uri.ashraf <uriashraf1@gmail.com>
2020-11-03 20:18:53 +0200

    Exercise 1,2,4,5

---
+++ callbacks-arrowfunctions/script.js
@@ -0,0 +1,64 @@
+
+// CallBack 
+
+// Exercise 1 
+
+
+// const pushPull = (func) => func()
+
+// const push = function (push) {
+//     console.log("pushing it!")
+//   }
+  
+//   const pull = function (pull) {
+//     console.log("pulling it!")
+//   }
+
+//   pushPull(push) 
+//   pushPull(pull) 
+
+ 
+
+// Exercise 2 
+
+// const returnTime = function (time) {
+//     alert('The current time is: ' + time)
+//   }
+  
+//   const time = new Date()
+  
+//   const getTime = (param) => {}
+  
+
+//   getTime(returnTime(time))
+  
+
+
+// Exercise 3 
+
+
+
+
+
+
+// Exercise 4 
+
+
+// const addSum = (num1, num2, num3) => console.log(num1 +num2 +num3) 
+
+
+// addSum(1,2,3)
+
+
+
+
+
+// Exercise 5 
+
+
+
+// const capitalizeFirstLetter= (string) => string.charAt(0).toUpperCase() + string.slice(1)
+  
+  
+//   console.log(capitalizeFirstLetter('hey you'))
+
