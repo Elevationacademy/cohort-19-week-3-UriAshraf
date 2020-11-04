1d65586b136eea567aa21bb4bd4720db42c1ec03
uri.ashraf <uriashraf1@gmail.com>
2020-11-04 17:48:51 +0200

    Exercises 1,2

---
+++ closures-modules/script.js
@@ -0,0 +1,60 @@
+// Closures & Modules
+
+
+// Exercise 1 
+
+
+
+// const StringFormatter = function(){
+
+// const capitalizeFirst = function(string){
+
+//    console.log( string.charAt(0).toUpperCase() + string.slice(1))
+    
+// }
+
+// const skewerCase = function(string) {
+
+//    console.log ( string.split(" ").join("-"))
+
+// }
+
+//     return {
+//         capital : capitalizeFirst,
+//         skewer : skewerCase, 
+//     }
+
+// }
+
+// const stringformatter = StringFormatter()
+
+// stringformatter.capital("narkis")
+// stringformatter.skewer("hey there beutifull lady")
+
+
+
+// Exercise 2 
+
+// const Bank = function(){
+//     let _money = 500
+
+// const depositCash = function(cash){
+
+//     _money += cash 
+
+// } 
+// const checkBalance = function(){
+//     console.log(_money)
+// }  
+// return {
+//     deposit : depositCash,
+//     showBalance : checkBalance
+// }
+
+// }
+
+// bank = Bank()
+
+// bank.deposit(200)
+// bank.deposit(250)
+// bank.showBalance()
